87e95d34912a9be7755a2a88f1381840c081b6be
Slava Slivka <aphelion.unet@gmail.net>
2017-12-06 22:43:28 +0200

    m4-t1

--- js/setup.js
+++ js/setup.js
@@ -113,6 +113,10 @@ var showSetup = function () {
 };
 
 var hideSetup = function () {
+  var children = setupSimilarList.children;
+  for (var i = children.length - 1; i >= 0; i--) {
+    setupSimilarList.removeChild(children[i]);
+  }
   setup.classList.add('hidden');
   document.removeEventListener('keydown', onSetupEscPress);
   setupWizard.removeEventListener('click', onSetupWizardClick);
